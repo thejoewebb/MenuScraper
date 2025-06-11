b88ecb89bd2900522e984b6a2c0a694bf81ea962
Joe Webb <joebarwebb@gmail.com>
2025-06-11 23:35:47 +0100

    Initial html test scrape working

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,23 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
 
 def scrape_tabelog_menu(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
+    #Load the page
+    response = requests.get(url, headers=HEADERS)
+    #print(response.status_code)
+    if response.status_code != 200:
+        print(f"Failed to load page: {response.status_code}")
+        return
+    
+    #save response for testing
+    with open("data/raw_html/test_page.html", "w", encoding="utf-8") as f:
+        f.write(response.text)
 
-    # Look for images
-    image_tags = soup.find_all("img")
+def process_html(path, output_dir="data/images"):
 
-    for i, img in enumerate(image_tags):
-        src = img.get("src")
-        if src and "menu" in src:  # crude filter
-            print(f"[{i}] {src}")
+    os.makedirs(output_dir, exist_ok=True)
+
+    with open(path, "r", encoding="utf-8") as f:
+        html = f.read()
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Find all <a> tags that wrap full-res images
+    links = soup.select("a.js-imagebox-trigger[href*='/images/'][href*='640x640']")
+
+    if len(links)>0:
+        for i, link in enumerate(links):
+            href = link.get("href")
+            try:
+                # Download the image
+                img_data = requests.get(href, headers=HEADERS).content
+                file_name = f"menu_{i:03}.jpg"
+                #file_path = os.path.join(output_dir, file_name)
+                print(file_name, href)
+                #with open(file_path, "wb") as f:
+                    #f.write(img_data)
+                #print(f"Saved: {file_name}")
+            except Exception as e:
+                print(f"Failed to download {href}: {e}")
+    else:
+        print("no images found")
 
 # Example restaurant (pick one manually from Tabelog to start)
 if __name__ == "__main__":
-    test_url = "https://tabelog.com/tokyo/A1301/A130101/13208304/"
-    scrape_tabelog_menu(test_url)
+    print("start")
+    test_url = "https://tabelog.com/tokyo/A1307/A130701/13275212/"
+    #scrape_tabelog_menu(test_url)
+    process_html("data/raw_html/test_page.html")
+    print("stop")
